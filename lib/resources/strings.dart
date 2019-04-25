8b4b2126acdd482ae4bec078ad8c6698cbf54f56
Diarmuid Ryan <diarmuidr3d@gmail.com>
2019-04-25 07:56:01 +0100

    add mins to strings

--- lib/resources/strings.dart
+++ lib/resources/strings.dart
@@ -1,4 +1,6 @@
 class Strings {
+  static Lang lang = Lang.EN;
+
   static const _search = {
     Lang.EN: "Search here for a stop",
     Lang.GA: "Cuardaigh stad anseo",
@@ -9,12 +11,30 @@ class Strings {
     Lang.GA: "Mo Ceanáin",
   };
 
+  static const _noResults = {
+    Lang.EN: "No results found for this stop 😭",
+    Lang.GA: "Níl aon torthaí don stad seo 😭",
+  };
+
+  static const _shortenedMinutes = {
+    Lang.EN: "min",
+    Lang.GA: "nóm",
+  };
+
   static String get search {
-    return _search[Lang.EN];
+    return _search[lang];
   }
 
   static String get myFavourites {
-    return _myFavourites[Lang.EN];
+    return _myFavourites[lang];
+  }
+
+  static String get noResults {
+    return _noResults[lang];
+  }
+
+  static String get shortenedMinutes {
+    return _shortenedMinutes[lang];
   }
 }
 
